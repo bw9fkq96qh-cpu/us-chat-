3f8c5660518271cd7ae035363cc18f5fd4f3d56a
Nishh <niteshh.012@gmail.com>
2026-05-07 03:38:38 +0500

    fixed frontend signup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,10 +19,15 @@ function AuthForm({ onAuth }) {
 
     try {
       const response = await fetch(`${API_URL}/api/${mode}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username })
-      });
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    username,
+    password
+  })
+});
       const data = await response.json().catch(() => ({}));
 
       if (!response.ok) {
