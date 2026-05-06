2ca1a2cb79b783308beb218feca5caefd5f60677
Nishh <niteshh.012@gmail.com>
2026-05-06 22:49:58 +0500

    clean imports

--- server/src/index.js
+++ server/src/index.js
@@ -170,10 +170,7 @@ io.on("connection", (socket) => {
   });
 });
 
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
